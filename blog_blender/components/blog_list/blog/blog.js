d35148358c9fb8bba952b5d1653994d9074f98e1
firas1awadallah <firasawa@gmail.com>
2023-08-28 17:50:23 +0000

    blog&bloglist

--- blog_blender/components/blog_list/blog/blog.js
+++ blog_blender/components/blog_list/blog/blog.js
@@ -1,3 +1,53 @@
+import React from 'react';
+
+
+
+
 export default function Blog(props) {
-    return(<img src={props.blog_photo}/>)
+    const handleBlogClick = () => {
+        if (props.onClick) {
+            props.onClick(); // Call the onClick function from the parent component
+        }
+    };
+
+    return (
+        <div
+            className="border rounded-lg shadow-md p-4 mb-4 w-80 h-40 flex bg-blue-50 cursor-pointer"
+            onClick={handleBlogClick} // Call the function when the component is clicked
+        >
+            {props.banner && <img src={props.banner} alt="Blog Cover" className="w-24 h-24 rounded mr-4" />}
+            <img src={props.blog_photo} alt="Blog Cover" className="w-24 h-24 rounded mr-4" />
+            <div>
+                <h3 className="font-semibold">{props.group_name}</h3>
+                <br /><hr />
+                <p className="text-gray-600">{props.description}</p>
+            </div>
+            <div>
+                {props.categories && props.categories.length > 0 && (
+                    <ul className="mt-2">
+                        {props.categories.map((category, index) => (
+                            <li key={index} className="text-gray-500">{category}</li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </div>
+    );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- blog_blender/pages/index.js
+++ blog_blender/pages/index.js
@@ -4,12 +4,32 @@ import Comment from "@/components/comment_list/comment/comment"
 import CommentList from "@/components/comment_list/comment_list"
 import { blog_data } from "@/data_samples/blog_list"
 import { comment_data } from "@/data_samples/comment_list"
+import BlogModal from "@/components/create_post/modal"
+import BlogDetailPage from "@/components/BlogDetailPage/BlogDetailPage";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CreateBlogModal from '@/components/create_blog/modal'
+import React, { useState } from 'react';
 export default function Home() {
+  const [selectedBlog, setSelectedBlog] = useState(null);
+
+  const handleBlogClick = (blog) => {
+    setSelectedBlog(blog);
+  };
   return (
     <main>
-      <p>Home</p>
-      {/* <BlogList/> */}
-      {/* <CommentList/> */}
+      <CreateBlogModal/>
+      <BlogModal/>
+      <BlogList onBlogClick={handleBlogClick} />
+      {selectedBlog && <BlogDetailPage blog={selectedBlog} />}
+
     </main>
   )
+
 }
+
+
+
+
+
+
+
