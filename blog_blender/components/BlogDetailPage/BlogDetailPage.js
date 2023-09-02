a3c16885403fe63f1482de1f8ccf811e879599f0
firas1awadallah <firasawa@gmail.com>
2023-09-02 15:56:02 +0000

    BlogDetailsPageStylie

--- blog_blender/components/BlogDetailPage/BlogDetailPage.js
+++ blog_blender/components/BlogDetailPage/BlogDetailPage.js
@@ -217,7 +217,7 @@ export default function BlogDetail({ blog }) {
       </section>
       <section className=" relative py-16 ">
         <div className=" container mx-auto px-4">
-          <div className=" bg-[#f2f2f2] relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
+          <div className=" bg-[#f2f2f2] relative flex flex-col min-w-0 break-words  w-full mb-6 shadow-xl rounded-lg -mt-64">
             <div className="px-6">
               <div className="flex flex-wrap justify-center">
                 <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
