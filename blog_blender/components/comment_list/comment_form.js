e0162b22e382e904d41517c17ab4529807204ff2
eman obeidat <amoonobeidat43@gmail.com>
2023-09-06 22:52:55 +0000

    edit comment style

--- blog_blender/components/comment_list/comment_form.js
+++ blog_blender/components/comment_list/comment_form.js
@@ -62,15 +62,15 @@ export default function CommentForm(props) {
   };
 
   return (
-    <div  class="bg-white rounded-2xl border border-solid border-gray-300 transform -translate-y-5 ">
+    <div  class="bg-white rounded-2xl border border-solid border-gray-500 transform -translate-y-5 mr-12  ml-14 mt-10 mb-10">
       
     
-    <section class="w-full flex px-4 py-2">
-      <div class="mr-1">
-        <img className="rounded-full w-8 h-8" src={props.userPic} alt="Profile Picture"/>
+    <section class="w-full flex px-3 py-2">
+      <div class="mr-2">
+        <img className="rounded-full w-10 h-10" src={props.userPic} alt="Profile Picture"/>
       </div>
       <div class="flex-1">
-        <textarea class="w-full p-2 bg-transparent outline-none placeholder-gray-600 text-black resize-none overflow-auto max-h-screen" onChange={update} rows="4" placeholder="What's happening?">{commentContent}</textarea>
+        <textarea class="w-full p-1/5 bg-transparent outline-none placeholder-gray-600 text-black resize-none overflow-auto max-h-screen" onChange={update} rows="4" placeholder="What's happening?">{commentContent}</textarea>
         <div style={{display:'flex', justifyContent:'flex-end'}} class="flex items-center pt-2 border-t border-blue">
           <div style={{display:'flex', justifyContent:'flex-end'}}> 
             <button style={{display:'flex',justifyContent:'flex-end',backgroundColor:'#0D9488'}} onClick={submitHandler} class="transition duration-500 ease-in-out bg-blue-500 bg-opacity-100  text-white text-opacity-100  py-2 px-3 rounded-2xl text-base font-bold focus:outline-none" >Send</button>
